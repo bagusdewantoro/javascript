eea1d568160527f270ed538df69d2829ca2059ab
bagusdewantoro <bagusdewantoro86@gmail.com>
2020-12-09 17:47:27 +0700

    assesment

---
+++ Silly Story Generator/main.js
@@ -0,0 +1,10 @@
+﻿const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
