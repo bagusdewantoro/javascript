8b0a6cd14bec4f3a837cd3669d70bdb6229f3f66
bagusdewantoro <bagusdewantoro86@gmail.com>
2020-12-13 00:49:10 +0700

    assesment

--- Silly Story Generator/main.js
+++ Silly Story Generator/main.js
@@ -8,3 +8,37 @@ function randomValueFromArray(array){
 }
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+
+randomize.addEventListener('click', result);
+
+function result() {
+let newStory = storyText;
+
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+
+newStory = newStory.replace(':insertx:', xItem);
+newStory = newStory.replace(':insertx:', xItem);
+newStory = newStory.replace(':inserty:', yItem);
+newStory = newStory.replace(':insertz:', zItem);
+
+  if(customName.value !== '') {
+    let name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300 / 14);
+    let temperature =  Math.round((94 - 32) * 5 / 9);
+    newStory = newStory.replace('94 fahrenheit', `${temperature} centigrade`);
+    newStory = newStory.replace('300 pounds', `${weight} stone`);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
