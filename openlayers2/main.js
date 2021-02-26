aa93606cde3b7818dea446e86dca2e6c540239f6
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-02-26 19:44:43 +0700

    ol geojson

--- openlayers2/main.js
+++ openlayers2/main.js
@@ -1,20 +1,22 @@
 import 'ol/ol.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import XYZSource from 'ol/source/XYZ';
-import {fromLonLat} from 'ol/proj';
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
 
 new Map({
   target: 'container-peta',
   layers: [
-    new TileLayer({
-      source: new XYZSource({
-        url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: './data/countriesBagus.json'
       })
     })
   ],
   view: new View({
-    center: fromLonLat([0, 0]),
+    center: [0, 0],
     zoom: 2
   })
 });
