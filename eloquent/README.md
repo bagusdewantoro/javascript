8c43e76ea6e93cfd36a7e0e2c55a84c33d5d0725
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-03-24 15:13:52 +0700

    eloquent

---
+++ eloquent/README.md
@@ -0,0 +1,8 @@
+Two ways to run the file, for example ch01values.js:
+
+    node ch01values.js
+
+    npm run 1
+
+
+See package.json "scripts" for reference
---
+++ eloquent/ch02structures.js
@@ -0,0 +1,8 @@
+// loop
+
+// input
+const prompt = require('prompt-sync')();
+const input1 = prompt('Masukkan angka 1: ');
+const angka1 = Number(input1);
+const angka2 = Number(prompt('Masukkan angka 2: '));
+console.log(angka1, ' + ', angka2,' = ', (angka1 + angka2));
