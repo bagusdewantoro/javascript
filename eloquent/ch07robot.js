e0293564e9253e30dcd73b86ece9449a81ae1668
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-07-09 18:29:17 +0700

    update

--- eloquent/ch07robot.js
+++ eloquent/ch07robot.js
@@ -42,12 +42,33 @@ const roadGraph = buildGraph(roads);
 //console.log(Object.values(roadGraph)[3])
 //console.log(roadGraph["Farm"]);
 
+// CURRENT LOCATION, UNDELIVERED PARCELS WITH ADDRESS
 class VillageState {
   constructor(place, parcels) {
     this.place = place;
     this.parcels = parcels;
   }
-  //move(destination) {
-    //if (!roadGraph[])
-  //}
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p;
+        return {place: destination, address: p.address};
+      }).filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
 }
+
+// TEST. ROBOT & PARCEL LOCATION
+let first = new VillageState(
+  "Post Office",
+  [{place: "Post Office", address: "Alice's House"}]
+);
+let next = first.move("Alice's House");
+
+console.log(first.place);
+console.log(first.parcels);
+console.log(next.place);
+console.log(next.parcels);
