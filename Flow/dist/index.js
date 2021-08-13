7c5ea64c361ca70f918354878e968842642393d8
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-08-14 00:14:24 +0700

    flow

---
+++ Flow/dist/index.js
@@ -0,0 +1,5 @@
+// VARIABLES
+let name = 'John Doe';
+let age = 40;
+let something = [1, 23];
+let users = ['John', 'Sara', 2];
---
+++ Flow/src/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+// VARIABLES
+let name: string = 'John Doe';
+let age: number = 40;
+let something: any = [1,23];
+let users: string[] = ['John', 'Sara', 2];
