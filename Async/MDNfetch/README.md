1fa0cb2ed7d2c9b3d2fb468d0df4aa189ba683bd
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-08-31 12:10:45 +0700

    async all

---
+++ Async/MDNfetch/README.md
@@ -0,0 +1,26 @@
+## MDN async - basic fetch
+
+"Partial async (async-1.js)" and "All async (async-all.js)".
+
+```
+npm init
+```
+
+Watch development
+```
+npm i nodemon
+```
+
+package.json
+```
+"scripts": {
+  "start": "node index",
+  "dev": "nodemon index"
+},
+```
+
+```
+npm run dev
+```
+
+Open http://localhost:5000/
---
+++ Async/MDNfetch/public/async-1.js
@@ -0,0 +1,19 @@
+console.log ('Console - 1');
+let image;
+
+fetch('coffee.jpg')
+.then((response) => {
+  console.log('Console - 2 - fetch');
+  return response.blob();
+})
+.then((myBlob) => {
+  let objectURL = URL.createObjectURL(myBlob);
+  image = document.createElement('img');
+  image.src = objectURL;
+  document.body.appendChild(image);
+})
+.catch((error) => {
+  console.log('There has been a problem with your fetch operation: ' + error.message);
+});
+
+console.log ('Console - 3' + image.src);
