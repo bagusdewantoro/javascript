1a03cdaa6a3eec37338ea5466108e47f0eab6163
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-09-01 16:17:00 +0700

    add if no task

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -2,13 +2,17 @@ const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const button = document.querySelector('button');
 const input = document.getElementById('item');
-const div = document.querySelector('div');
 
 let para = document.createElement('p');
 para.textContent = "You don't have any task yet";
 
+// Set local Storage:
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
+
 // Check whether there is a task or not
-window.onload = () => {
+window.addEventListener('load', () => {
   if (ul.hasChildNodes()) {
     if (para.parentElement === ul) {
       ul.removeChild(para);
@@ -16,14 +20,14 @@ window.onload = () => {
   } else {
     ul.appendChild(para);
   }
-};
+});
 
-// Set local Storage:
-let itemsArray = localStorage.getItem('items')
-  ? JSON.parse(localStorage.getItem('items'))
-  : [];
-localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+// Generate List function
+const genLi = (text) => {
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
+}
 
 // Add Task using form to local storage
 form.addEventListener('submit', (e) => {
@@ -42,14 +46,7 @@ form.addEventListener('submit', (e) => {
   }
 });
 
-// Generate List function
-const genLi = (text) => {
-  const li = document.createElement('li');
-  li.textContent = text;
-  ul.appendChild(li);
-}
-
-data.forEach((item) => {
+data.forEach(item => {
   genLi(item);
 });
 
@@ -59,5 +56,6 @@ button.addEventListener('click', () => {
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   }
+  itemsArray = [];
   ul.appendChild(para);
 });
