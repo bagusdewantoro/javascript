207442a1419f27311e2b32ae460e343ab68fc853
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-09-01 13:35:13 +0700

    local storage

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -2,39 +2,62 @@ const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const button = document.querySelector('button');
 const input = document.getElementById('item');
+const div = document.querySelector('div');
 
-// Local Storage:
+let para = document.createElement('p');
+para.textContent = "You don't have any task yet";
+
+// Check whether there is a task or not
+window.onload = () => {
+  if (ul.hasChildNodes()) {
+    if (para.parentElement === ul) {
+      ul.removeChild(para);
+    }
+  } else {
+    ul.appendChild(para);
+  }
+};
+
+// Set local Storage:
 let itemsArray = localStorage.getItem('items')
   ? JSON.parse(localStorage.getItem('items'))
   : [];
 localStorage.setItem('items', JSON.stringify(itemsArray));
 const data = JSON.parse(localStorage.getItem('items'));
 
-// Generate List
+// Add Task using form to local storage
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // Local Storage:
+  itemsArray.push(input.value);
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  // Remove 'You don't have any task yet' text
+  if (para.parentElement === ul) {
+    ul.removeChild(para);
+    genLi(input.value);
+    input.value = '';
+  } else {
+    genLi(input.value);
+    input.value = '';
+  }
+});
+
+// Generate List function
 const genLi = (text) => {
   const li = document.createElement('li');
   li.textContent = text;
   ul.appendChild(li);
 }
 
-// Add Task
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // Local Storage:
-  itemsArray.push(input.value);
-  localStorage.setItem('items', JSON.stringify(itemsArray));
-  genLi(input.value);
-  input.value = '';
-});
-
 data.forEach((item) => {
   genLi(item);
 });
 
-// CLEAR BUTTON
+// Clear Button
 button.addEventListener('click', () => {
   localStorage.clear();
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   }
+  ul.appendChild(para);
 });
