e4310bd21e0963fb626a8a481855e849b7421e23
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-09-08 12:20:25 +0700

    update

--- ExpenseTracker/Vanilla/script.js
+++ ExpenseTracker/Vanilla/script.js
@@ -8,24 +8,52 @@ amount = document.getElementById('amount');
 
 // dummy transactions
 let transactions = [
-  {id: 1, text: 'makan', amount: '-2000'},
-  {id: 2, text: 'gaji', amount: '10000'}
+  {id: 1, text: 'kamera', amount: "-2000"},
+  {id: 2, text: 'jual', amount: "10000"}
 ];
 
-// create li, append to ul
+// function 1000 separator
+const thousands = (num) => {
+  /*
+  // with cent decimal
+  const num_parts = num.toString().split(".");
+  num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return num_parts.join(".");
+  */
+  // withount cent decimal
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+// function create li, append to ul
 const listHistory = (transItem) => {
   const item = document.createElement('li');
   item.classList.add(transItem.amount < 0 ? 'minus' : "plus");
   item.innerHTML = `
     <button id="${transItem.id}" class="delete-btn">x</button>
     ${transItem.text}
-    <span>${transItem.amount < 0 ? "-" : "+"}$${Math.abs(transItem.amount)}</span>
+    <span>${transItem.amount < 0 ? "-" : "+"} Rp ${thousands(Math.abs(transItem.amount))}</span>
   `;
   list.appendChild(item);
 }
 
-// map existing data to DOM
-transactions.map(eachItem => listHistory(eachItem));
+// function calculate
+const calculateValue = () => {
+  // balance
+  const totalAmount = transactions.reduce((current, next) => current + Number(next.amount), 0);
+  balance.textContent = `Rp ${thousands(totalAmount)}`;
+  // income
+  const plusAmount = transactions.reduce((current, next) => {
+    if (next.amount < 0) return Number(current);
+    return current + Number(next.amount);
+  }, 0);
+  income.textContent = `+ Rp ${thousands(plusAmount)}`;
+  // expense
+  const minusAmount = transactions.reduce((current, next) => {
+    if (next.amount > 0) return Number(current);
+    return current + Number(next.amount);
+  }, 0);
+  expense.textContent = `- Rp ${thousands(Math.abs(minusAmount))}`;
+}
 
 // get text & amount value, store in history list
 const addTransaction = (e) => {
@@ -38,8 +66,10 @@ const addTransaction = (e) => {
       amount: amount.value
     };
     transactions.push(transaction);
-    // add new data to DOM
+
     listHistory(transaction);
+    calculateValue();
+
     // refresh
     text.value = '';
     amount.value = '';
@@ -48,5 +78,11 @@ const addTransaction = (e) => {
     alert('Please fill the data')
   )
 };
+
+// map existing data to DOM
+transactions.map(eachItem => listHistory(eachItem));
+
+// calculate existing value
+calculateValue();
 
 form.addEventListener('submit', addTransaction);
