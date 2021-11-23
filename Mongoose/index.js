701b4c7fe11ec5c09f2442e035d2d9808fd931f4
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-11-23 17:41:39 +0700

    update

---
+++ Mongoose/index.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://netninja:test1234@nodetuts.y6vzw.mongodb.net/docs?retryWrites=true&w=majority', () => console.log('connected to DB'));
+
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+
+  kittySchema.methods.speak = function speak() {
+    const greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  };
+
+  const Kitten = mongoose.model('Kitten', kittySchema);
+
+  const silence = new Kitten({ name: 'Silence' });
+  console.log(silence.name);
+
+  const fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.speak();
+
+  const kittens = await Kitten.find();
+  console.log(kittens);
+}
+
