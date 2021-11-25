1250eedf210cd83220ae49fac6896562452c08e4
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-11-25 16:59:24 +0700

    .

---
+++ MERN/Expense/client/src/expense5Negative/context/AppReducer.js
@@ -0,0 +1,21 @@
+const myReducer = (state, action) => {
+  switch(action.type) {
+
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(item => item.id !== action.payload)
+      }
+
+    case 'ADD_TRANSACTION':
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions]
+      }
+
+    default:
+      return state;
+  }
+};
+
+export default myReducer;
