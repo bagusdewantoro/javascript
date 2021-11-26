7a4f7a60923d6a95073f8bcbe550bb1e61831aa4
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-11-26 13:43:16 +0700

    socket

---
+++ Socket/Chat/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
