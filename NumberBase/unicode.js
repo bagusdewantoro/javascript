6a35902e7f2a991861f1ed1bad07adb878a5c66e
bagusdewantoro <bagusdewantoro86@gmail.com>
2021-12-07 11:30:53 +0700

    number base

---
+++ NumberBase/unicode.js
@@ -0,0 +1,7 @@
+console.log('\u0402');
+console.log('Ђ'.charCodeAt(0));
+console.log('Ђ'.charCodeAt(0).toString(16));
+
+console.log('\u0061');
+console.log('a'.charCodeAt(0));
+console.log('a'.charCodeAt(0).toString(16));
