7d9556fc3bf1bc5c25d481a39f4484c6bd9b7bdf
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-01-31 00:11:46 +0700

    auth

---
+++ MERN/Memories2/server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use('/posts', postRoutes); // must be after cors
+
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`sukses di port ${PORT}`)))
+  .catch((error) => console.log(error.message));
