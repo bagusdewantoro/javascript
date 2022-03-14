4cf59a708e68b4a13ac0a32c1ee118bb14d5d136
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-03-15 00:04:43 +0700

    graphql

---
+++ Graphql/NetNinja/server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema
+}));
+
+app.listen(4000, () => console.log('listening on port 4000'));
