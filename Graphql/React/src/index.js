5e73f48c7652a026e34e5667e5e88d91ab010203
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-04-01 00:23:39 +0700

    react gql

--- Graphql/React/src/index.js
+++ Graphql/React/src/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 
-ReactDOM.render(
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+// ====== react 18 way
+const container = document.getElementById('root');
+// create a root:
+const root = ReactDOMClient.createRoot(container);
+// initial render: render an element to the root:
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+)
+
+// =================
+
+// ====== before react 18
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ApolloProvider client={client}>
+//       <App />
+//     </ApolloProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
