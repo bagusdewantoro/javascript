d2bd40c4145af597bb3a7ab3fd18d84b869ff9cb
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-04-07 22:34:54 +0700

    without express

--- Loiane/index.js
+++ Loiane/index.js
@@ -1,6 +1,18 @@
-const express = require('express');
-const app = express();
+// // 1. server with express
+// const express = require('express');
+// const app = express();
+// app.listen();
+// const stack = require('./stack');
+
+// // 2. server with core module
+// // 2.a
+// const http = require('http');
+// const server = http.createServer();
+// server.listen();
+// // 2.b
+require('http')
+  .createServer()
+  .listen();
+
 
 const stack = require('./stack');
-
-app.listen();
