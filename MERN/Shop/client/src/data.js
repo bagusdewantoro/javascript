83808d3e87fc5fc4dbfd4b8f7bf703c2b633e1eb
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-05-18 00:45:57 +0700

    products

--- MERN/Shop/client/src/data.js
+++ MERN/Shop/client/src/data.js
@@ -33,3 +33,56 @@ export const sliderItems = [
     bg: 'black',
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://images.pexels.com/photos/2646825/pexels-photo-2646825.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    title: 'ELECTRIC GUITAR'
+  },
+  {
+    id: 2,
+    img: 'https://images.pexels.com/photos/3714523/pexels-photo-3714523.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    title: 'ACOUSTIC GUITAR'
+  },
+  {
+    id: 3,
+    img: 'https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    title: 'CLASSIC GUITAR'
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: 'https://www.pngarts.com/files/3/Red-Electric-Guitar-Transparent-Background-PNG.png'
+  },
+  {
+    id: 2,
+    img: 'https://www.pngarts.com/files/1/Rock-Guitar-Transparent-Background-PNG.png'
+  },
+  {
+    id: 3,
+    img: 'https://www.pngarts.com/files/1/Rock-Guitar-Transparent-Images.png'
+  },
+  {
+    id: 4,
+    img: 'https://www.pngarts.com/files/3/Electric-Guitar-PNG-High-Quality-Image.png'
+  },
+  {
+    id: 5,
+    img: 'https://www.pngarts.com/files/3/Wooden-Guitar-Download-PNG-Image.png'
+  },
+  {
+    id: 6,
+    img: 'https://www.pngarts.com/files/1/Acoustic-Guitar-Transparent-Image.png'
+  },
+  {
+    id: 7,
+    img: 'https://www.pngarts.com/files/1/Guitar-Transparent-Images-1.png'
+  },
+  {
+    id: 8,
+    img: 'https://www.pngarts.com/files/1/Guitar-PNG-Photo.png'
+  },
+];
--- MERN/Shop/client/src/pages/Home.js
+++ MERN/Shop/client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Slider from '../components/Slider';
+import Categories from '../components/Categories';
+import Products from '../components/Products';
 
 const Home = () => {
   return (
@@ -8,6 +10,8 @@ const Home = () => {
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
+      <Products />
     </div>
 
   )
