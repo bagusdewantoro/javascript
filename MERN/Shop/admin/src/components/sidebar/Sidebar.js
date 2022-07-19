8f23de1417db2f26cf636c878fe079a6669cb126
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-07-19 16:28:51 +0700

    sidebar refactor: COMPONENT AS PROPS!

--- MERN/Shop/admin/src/components/sidebar/Sidebar.js
+++ MERN/Shop/admin/src/components/sidebar/Sidebar.js
@@ -2,31 +2,82 @@ import './sidebar.css';
 import {
   LineStyle,
   Timeline,
-  TrendingUp
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutline,
+  Report,
 } from '@material-ui/icons';
 
+
 export const Sidebar = () => {
+  const menuData = [
+    {
+      title: 'Dashboard',
+      submenu: [
+        {icon: LineStyle, name: 'Home'},
+        {icon: Timeline, name: 'Analytics'},
+        {icon: TrendingUp, name: 'Sales'},
+      ]
+    },
+    {
+      title: 'Quick Menu',
+      submenu: [
+        {icon: PermIdentity, name: 'Users'},
+        {icon: Storefront, name: 'Product'},
+        {icon: AttachMoney, name: 'Transactions'},
+        {icon: BarChart, name: 'Reports'},
+      ]
+    },
+    {
+      title: 'Notifications',
+      submenu: [
+        {icon: MailOutline, name: 'Mail'},
+        {icon: DynamicFeed, name: 'Feedback'},
+        {icon: ChatBubbleOutline, name: 'Messages'},
+      ]
+    },
+    {
+      title: 'Staff',
+      submenu: [
+        {icon: WorkOutline, name: 'Manage'},
+        {icon: Timeline, name: 'Analytics'},
+        {icon: Report, name: 'Reports'},
+      ]
+    },
+  ]
   return (
     <div className='sidebar'>
       <div className='sidebarWrapper'>
-        <div className='sidebarMenu'>
-          <h3 className='sidebarTitle'>Dashboard</h3>
-          <ul className='sidebarList'>
-            <li className='sidebarListItem'>
-              <LineStyle className='sidebarIcon' />
-              Home
-            </li>
-            <li className='sidebarListItem'>
-              <Timeline className='sidebarIcon' />
-              Analytics
-            </li>
-            <li className='sidebarListItem'>
-              <TrendingUp className='sidebarIcon' />
-              Sales
-            </li>
-          </ul>
-        </div>
+        {menuData.map((menu, index) => <SideBarTitle key={index} title={menu.title} menus={menu.submenu}/> )}
       </div>
     </div>
-    )
-  }
+  )
+}
+
+
+const SideBarTitle = ({ title, menus }) => {
+  return (
+    <div className='sidebarMenu'>
+      <h3 className="sidebarTitle">{title}</h3>
+      {menus.map((el, index) => <SubMenu key={index} icon={el.icon} name={el.name} />)}
+    </div>
+  )
+}
+
+
+const SubMenu = ({icon: Icon, name}) => {
+  return (
+    <ul className='sidebarList'>
+      <li className="sidebarListItem">
+        <Icon className="sidebarIcon" />
+        {name}
+      </li>
+    </ul>
+  )
+}
