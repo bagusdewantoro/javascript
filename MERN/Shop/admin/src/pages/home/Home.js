8c24e191e6be1007497b9380e44617e30f5fc2bd
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-09-21 00:11:14 +0700

    .

--- MERN/Shop/admin/src/pages/home/Home.js
+++ MERN/Shop/admin/src/pages/home/Home.js
@@ -7,8 +7,36 @@ import { Chart } from '../../components/chart/Chart'
 import { userData } from '../../dummyData'
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { userRequest } from '../../requestMethods';
+import { useState, useEffect, useMemo } from 'react';
 
 export const Home = () => {
+  const [ userStats, setUserStats ] = useState([])
+  const MONTHS = useMemo(() => [
+    'Jan', 'Feb', 'Mar',
+    'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep',
+    'Oct', 'Nov', 'Dev'
+  ], [])
+
+  useEffect(() => {
+    (async() => {
+      try {
+        const res = await userRequest.get('/users/stats')
+        res.data.map(item => {
+          setUserStats(prev => [
+            ...prev,
+            {
+              name: MONTHS[item._id - 1],
+              'Active User': item.total,
+            },
+          ])
+        })
+      } catch {}
+    })()
+  }, [MONTHS])
+  console.log(userStats)
+
   return (
     <div className='home'>
       <FeaturedInfo />
