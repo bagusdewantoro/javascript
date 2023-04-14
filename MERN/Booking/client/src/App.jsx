c46a33d813ea37255df81ad6e14b422bbef695bd
Bagus Dewantoro <bagusdewantoro@gmail.com>
2023-04-14 22:49:16 +0700

    Client & API - Login & Register Basic

--- MERN/Booking/client/src/App.jsx
+++ MERN/Booking/client/src/App.jsx
@@ -6,19 +6,22 @@ import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import Layout from "./Layout";
 import axios from 'axios'
+import {UserContextProvider} from './UserContext'
 
 axios.defaults.baseURL = "http://localhost:4000"
 axios.defaults.withCredentials = true
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
+  	<UserContextProvider>
+	    <Routes>
+	      <Route path="/" element={<Layout />}>
+	        <Route index element={<IndexPage />} />
+	        <Route path="/login" element={<LoginPage />} />
+	        <Route path="/register" element={<RegisterPage />} />
+	      </Route>
+	    </Routes>
+  	</UserContextProvider>
   )
 }
 
