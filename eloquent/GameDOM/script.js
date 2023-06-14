930a59bd6ee70d64ec665d50889e6fa7ca61219d
Bagus Dewantoro <bagusdewantoro@gmail.com>
2023-06-14 13:18:53 +0700

    Revert "Platform Game - Player (state, vector & refactor display)"
    This reverts commit da5da73bb4a67587042f214972955433134df334.

--- eloquent/GameDOM/script.js
+++ eloquent/GameDOM/script.js
@@ -14,50 +14,11 @@ let simpleLevelPlan = `
 ......##############..
 ......................`;
 
-class State {
-  constructor(level, actors) {
-    this.level = level;
-    this.actors = actors;
-  }
-  static start(level) {
-    return new State(level, level.startActors);
-  }
-  get player() {
-    return this.actors.find(a => a.type == "player");
-  }
-  update(time) {
-    let actors = this.actors.map(actor => actor.update(time, this));
-    return new State(this.level, actors);
-  };
-}
-
-class Vec {
-  constructor(x, y) {
-    this.x = x
-    this.y = y
-  }
-  plus(other) {
-    return new Vec(this.x + other.x,  this.y + other.y)
-  }
-}
-
-class Player {
-  constructor(pos, speed) {
-    this.pos = pos        // Vec object
-    this.speed = speed;
-  }
-  get type() { return "player" }
-  static create(pos) {
-    return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0))
-  }
-  size = new Vec(0.8, 1.5);
-}
-
 const levelChars = {
   ".": "empty", 
   "#": "wall", 
   "+": "lava",
-  "@": Player, 
+  "@": "player", 
   "o": "coin",
   "=": "lava", 
 };
@@ -67,14 +28,10 @@ class Level {
     let rows = plan.trim().split("\n").map(l => [...l])
     this.height = rows.length
     this.width = rows[0.].length
-    this.startActors = []
     this.rows = rows.map((row, y) => {
       return row.map((ch, x) => {
         let type = levelChars[ch]
         if (typeof type == "string") return type
-        this.startActors.push(
-          type.create(new Vec(x, y), ch)
-        )
         return "empty"
       })
     })
@@ -103,54 +60,23 @@ function elt(name, attrs, ...children) {
   return dom;
 }
 
-class DOMDisplay {
-  constructor(parent, level) {
-    this.dom = elt("div", {class: "game"}, drawGrid(level));
-    parent.appendChild(this.dom);
-  }
-  syncState(state) {
-    this.actorLayer = drawActors(state.actors);
-    this.dom.appendChild(this.actorLayer);
-  }
-}
-
-function drawGrid(level) {
-  return elt("table", {
-    class: "background",
-    style: `width: ${level.width * scale}px`
-  }, ...level.rows.map(row =>
-    elt("tr", {style: `height: ${scale}px`},
-        ...row.map(type => elt("td", {class: type})))
-  ));
-}
-
-function drawActors(actors) {
-  return elt("div", {}, ...actors.map(actor => {
-    let rect = elt("div", {class: `actor ${actor.type}`});
-    rect.style.width = `${actor.size.x * scale}px`;
-    rect.style.height = `${actor.size.y * scale}px`;
-    rect.style.left = `${actor.pos.x * scale}px`;
-    rect.style.top = `${actor.pos.y * scale}px`;
-    return rect;
-  }));
-}
-
-function runAnimation(frameFunc) {
-  let lastTime = null;
-  function frame(time) {
-    let timeStep = Math.min(time - lastTime, 100) / 1000;
-    if (frameFunc(timeStep) === false) return;
-    requestAnimationFrame(frame);
-  }
-  requestAnimationFrame(frame);
-}
-
-function runLevel(level, Display) {
-  let display = new Display(document.body, level);
-  let state = State.start(level);
-  runAnimation(() => display.syncState(state))
-}
-
-runLevel(new Level(simpleLevelPlan), DOMDisplay);
-
+document.body.appendChild(elt(
+  "div",                              // name
+  {class: "game"},                    // attrs
+  elt(                                // ...children
+    "table",                              // name
+    {                                     // attrs
+      class: "background",
+      style: `width: ${new Level(simpleLevelPlan).width * scale}px`
+    }, 
+    ...new Level(simpleLevelPlan).rows.map(row => elt(       // ...children
+      "tr",                                   // name
+      {style: `height: ${scale}px`},          // attrs
+      ...row.map(type => elt(                 // ...children
+        "td",                                     // name
+        {class: type}                             // attrs
+      ))
+    ))
+  )
+))
 
